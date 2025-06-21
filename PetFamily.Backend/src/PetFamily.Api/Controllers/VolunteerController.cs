9a91ff68375a7c5a0ef6014075317c5aa9b5cdce
Nikita Kuptsov <verfixyo@gmail.com>
2025-06-21 21:01:02 +0400

    Add auto validation and CustomResultFactory

--- PetFamily.Backend/src/PetFamily.Api/Controllers/VolunteerController.cs
+++ PetFamily.Backend/src/PetFamily.Api/Controllers/VolunteerController.cs
@@ -1,4 +1,3 @@
-using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 using PetFamily.Api.Extensions;
 using PetFamily.Application.Volunteers.CreateVolunteer;
@@ -10,20 +9,14 @@ public class VolunteerController : ApplicationController
     [HttpPost]
     public async Task<ActionResult<Guid>> Create(
         [FromServices] CreateVolunteerHandler handler,
-        [FromServices] IValidator<CreateVolunteerRequest> validator,
         [FromBody] CreateVolunteerRequest request,
         CancellationToken cancellationToken = default)
     {
-        var validationResult = await validator.ValidateAsync(request, cancellationToken);
-
-        if (!validationResult.IsValid)
-            return validationResult.ToValidationErrorResponse();
-        
         var result = await handler.HandleAsync(request, cancellationToken);
 
         if (result.IsFailure)
             return result.Error.ToResponse();
 
-        return CreatedAtAction("", result.Value);
+        return Ok(result.Value);
     }
 }
