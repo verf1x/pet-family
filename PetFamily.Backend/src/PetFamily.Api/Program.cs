48ac377641a5c34e23637d1b65da37ab699c68f9
Nikita Kuptsov <82285281+verf1x@users.noreply.github.com>
2025-06-21 21:01:02 +0400

    Add auto validation and CustomResultFactory

--- PetFamily.Backend/src/PetFamily.Api/Program.cs
+++ PetFamily.Backend/src/PetFamily.Api/Program.cs
@@ -1,5 +1,7 @@
+using PetFamily.Api.Validation;
 using PetFamily.Application;
 using PetFamily.Infrastructure;
+using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,10 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructure();
 builder.Services.AddApplication();
 
+builder.Services.AddFluentValidationAutoValidation(configuration =>
+{
+    configuration.OverrideDefaultResultFactoryWith<CustomResultFactory>();
+});
 
 var app = builder.Build();
 
