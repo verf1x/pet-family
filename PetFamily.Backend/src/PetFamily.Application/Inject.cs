4a766dbe825e2bca81654d49b08a86d219e9e059
Nikita Kuptsov <verfixyo@gmail.com>
2025-07-11 13:38:32 +0400

    Remove test FileController with handlers

--- PetFamily.Backend/src/PetFamily.Application/Inject.cs
+++ PetFamily.Backend/src/PetFamily.Application/Inject.cs
@@ -1,8 +1,5 @@
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
-using PetFamily.Application.Files.GetPresignedUrl;
-using PetFamily.Application.Files.Remove;
-using PetFamily.Application.Files.Upload;
 using PetFamily.Application.Volunteers.AddPet;
 using PetFamily.Application.Volunteers.Create;
 using PetFamily.Application.Volunteers.Delete;
@@ -19,9 +16,6 @@ public static class Inject
         services.AddScoped<HardDeleteVolunteerHandler>();
         services.AddScoped<SoftDeleteVolunteerHandler>();
         services.AddScoped<AddPetHandler>();
-        // services.AddScoped<UploadFileHandler>();
-        services.AddScoped<RemoveFileHandler>();
-        services.AddScoped<GetPresignedUrlHandler>();
         
         services.AddValidatorsFromAssembly(typeof(Inject).Assembly);
         
