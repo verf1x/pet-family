52babb0a989ca027125fe7b6e6e06fe0f3f3911f
Nikita Kuptsov <verfixyo@gmail.com>
2025-07-26 16:31:49 +0400

    Refactor validators

--- PetFamily.Backend/src/PetFamily.Application/Dtos/Validators/UploadFileDtoValidator.cs
+++ PetFamily.Backend/src/PetFamily.Application/Dtos/Validators/UploadFileDtoValidator.cs
@@ -11,8 +11,11 @@ public class UploadFileDtoValidator :  AbstractValidator<UploadFileDto>
         RuleFor(v => v.FileName)
             .NotEmpty()
             .WithError(Errors.General.ValueIsRequired());
-        
+
         RuleFor(v => v.Content)
-            .Must(c => c.Length is > 0 and < 10485760);
+            .Must(c => c.Length is > 0 and < 10485760)
+            .WithError(Error.Validation("validation.fileSize",
+                "File size must be between 1 byte and 10 MB",
+                nameof(UploadFileDto.Content.Length)));
     }
 }
