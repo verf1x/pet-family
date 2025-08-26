c022b33532b2456a25a35ae64870edf5cb6f6716
Nikita Kuptsov <82285281+verf1x@users.noreply.github.com>
2025-08-27 00:02:33 +0400

    Add MovePetHandlerTest

---
+++ PetFamily.Backend/tests/PetFamily.Application.IntegrationTests/Volunteers/MovePetHandlerTest.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PetFamily.Application.Abstractions;
+using PetFamily.Application.VolunteersManagement.UseCases.MovePet;
+using PetFamily.TestUtils;
+
+namespace PetFamily.Application.IntegrationTests.Volunteers;
+
+public class MovePetHandlerTest : VolunteerTestBase
+{
+    private readonly ICommandHandler<int, MovePetCommand> _sut;
+
+    public MovePetHandlerTest(IntegrationTestsWebFactory factory)
+        : base(factory)
+    {
+        _sut = Scope.ServiceProvider.GetRequiredService<ICommandHandler<int, MovePetCommand>>();
+    }
+
+    [Fact]
+    public async Task HandleAsync_ShouldMovePetToNewPosition_WhenCommandIsValid()
+    {
+        // Arrange
+        var volunteer = await VolunteerSeeder.SeedVolunteerAsync(VolunteersRepository, WriteDbContext);
+        var species = await SpeciesSeeder.SeedSpeciesAsync(SpeciesRepository, WriteDbContext);
+        var pet1 = await VolunteerSeeder.SeedPetAsync(WriteDbContext, volunteer, species.Id, species.Breeds[0].Id);
+        var pet2 = await VolunteerSeeder.SeedPetAsync(WriteDbContext, volunteer, species.Id, species.Breeds[1].Id);
+
+        var command = new MovePetCommand(volunteer.Id, pet1.Id.Value, 2);
+
+        // Act
+        var result = await _sut.HandleAsync(command, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().Be(2);
+        var updatedVolunteer = await WriteDbContext.Volunteers.FirstOrDefaultAsync();
+        updatedVolunteer!.Pets.Should().HaveCount(2);
+        updatedVolunteer.Pets[0].Position.Value.Should().Be(2);
+        updatedVolunteer.Pets[1].Position.Value.Should().Be(1);
+    }
+}
