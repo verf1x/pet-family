5a73fda835661daf6e00087449e3442df261354e
Nikita Kuptsov <82285281+verf1x@users.noreply.github.com>
2025-08-28 00:21:43 +0400

    Add GetBreedsBySpeciesIdHandlerTest

---
+++ PetFamily.Backend/tests/PetFamily.Application.IntegrationTests/Species/GetBreedsBySpeciesIdHandlerTest.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using PetFamily.Application.Abstractions;
+using PetFamily.Application.SpeciesManagement.Queries.GetBreedsBySpeciesId;
+using PetFamily.Contracts.Dtos.Species;
+using PetFamily.TestUtils;
+
+namespace PetFamily.Application.IntegrationTests.Species;
+
+public class GetBreedsBySpeciesIdHandlerTest : SpeciesTestBase
+{
+    private readonly IQueryHandler<IReadOnlyList<BreedDto>, GetBreedsBySpeciesIdQuery> _sut;
+
+    public GetBreedsBySpeciesIdHandlerTest(IntegrationTestsWebFactory factory)
+        : base(factory)
+    {
+        _sut = Scope.ServiceProvider
+            .GetRequiredService<IQueryHandler<IReadOnlyList<BreedDto>, GetBreedsBySpeciesIdQuery>>();
+    }
+
+    [Fact]
+    public async Task HandleAsync_ShouldReturnBreeds_WhenSpeciesExists()
+    {
+        // Arrange
+        var species = await SpeciesSeeder.SeedSpeciesWithBreedsAsync(SpeciesRepository, WriteDbContext);
+
+        var query = new GetBreedsBySpeciesIdQuery(species.Id);
+
+        // Act
+        var result = await _sut.HandleAsync(query, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().HaveCount(species.Breeds.Count);
+
+        foreach (var breed in species.Breeds)
+            result.Value.Should().Contain(b => b.Id == breed.Id.Value && b.Name == breed.Name);
+    }
+}
